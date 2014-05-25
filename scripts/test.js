b857105f96d834d8e7e2e95da6c124366b3b348a
rWolstenholme <ryanwolstenholme@gmail.com>
2014-05-26 05:32:03 +1200

    There's an axis
    Whew d3 doesn't make this too easy

--- scripts/test.js
+++ scripts/test.js
@@ -4,21 +4,19 @@ var dataSuffix = "-Table1.csv";
 var dataPrefix = "http://www.ecs.vuw.ac.nz/~stuart";
 var redraw = dispRank;
 window.onresize = delayedRedraw;
+var width, height;
 
 function startTest() {
-    var canvas = document.getElementById("canvas");
-    var width = canvas.clientWidth;
-    var height = canvas.clientHeight;
     years = [];
     for (var i = yearStart; i <= yearEnd; i++) {
         var dataLoc = "data/" + i + dataSuffix;
-        var year = [];
-        d3.csv(dataLoc, function (games) {
-            console.log(games.length);
-            games = games.filter(function(game){return game.Date.lastIndexOf("BYES", 0) !== 0;});
-            console.log(games.length);
-            
-            years.push(games.map(function(game){
+        var method = (i === 2009) ? d3.tsv : d3.csv;
+        method(dataLoc, function (games) {
+            games = games.filter(function (game) {
+                return game.Date.lastIndexOf("BYES", 0) !== 0;
+            });
+
+            years.push(games.map(function (game) {
                 var scoreSplit = game.Score.split("–");
                 var time = game.time;
                 return {
@@ -32,11 +30,54 @@ function startTest() {
                 }
             }));
         });
-    }
+    };
+    updateDims();
+    redraw();
 }
 
 function dispRank() {
     redraw = dispRank;
+    var oldCanv = document.getElementById("canvas");
+    if (oldCanv) oldCanv.parentNode.removeChild(oldCanv);
+    
+    var bigMargin = 50;
+    var con = {xS:bigMargin,xB:width-bigMargin,yS:bigMargin,yB:height-bigMargin,w:width-bigMargin*2,h:height-bigMargin*2};
+
+    var xAxisScale = d3.scale.linear()
+        .domain([1, 17])
+        .range([0, con.w]);
+
+    var yAxisScale = d3.scale.linear()
+        .domain([0, 10])
+        .range([con.h, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(xAxisScale)
+        .ticks(17)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(yAxisScale)
+        .ticks(10)
+        .orient("left");
+
+    var svg = d3.select("body").append("svg")
+        .attr("id", "canvas")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + con.xS + "," + con.yS + ")");
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + con.h + ")")
+        .call(xAxis);
+
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + 0 + ",0)")
+        .call(yAxis);
 }
 
 function dispGeo() {
@@ -52,8 +93,8 @@ function dispExpl() {
 }
 
 function updateDims() {
-    width = canvas.clientWidth;
-    height = canvas.clientHeight;
+    width = document.body.clientWidth;
+    height = window.innerHeight - 120;
 }
 
 function changeSelected(toBeSelected) {
@@ -67,6 +108,7 @@ function changeSelected(toBeSelected) {
 //Here downwards pretty much copied from CMS @ http://stackoverflow.com/questions/2854407/javascript-jquery-window-resize-how-to-fire-after-the-resize-is-completed
 function delayedRedraw() {
     delay(function () {
+        updateDims();
         redraw();
     }, 500);
 }
